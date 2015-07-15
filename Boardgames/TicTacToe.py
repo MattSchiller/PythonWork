18f93e8918701505a69dd8de1e76e315a25d7fac
MattSchiller <matt.s.schiller@gmail.com>
2015-07-15 16:32:47 -0400

    Modified TTT for AI. AI not working as intended.

--- Boardgames/TicTacToe.py
+++ Boardgames/TicTacToe.py
@@ -1,6 +1,8 @@
-#python
+#Matt Schiller's TicTacToe Program
+from colorama import Fore, Style 
 
 def printBoard(myBoard):
+	#Prints the given board state.
 	print((str(myBoard[0]) +"|" +str(myBoard[1]) +"|" +str(myBoard[2])).center(40))
 	print(("-+-+-").center(40))
 	print((str(myBoard[3]) +"|" +str(myBoard[4]) +"|" +str(myBoard[5])).center(40))
@@ -9,63 +11,68 @@ def printBoard(myBoard):
 	print("") 
 	
 def checkForWin(myBoard):
-	global Winner
-	for j in range(0,3):
-		if (myBoard[j] is not " ") and (
-		#Rows and Cols
-			((myBoard[j] == myBoard[j+1] and myBoard[j+1] == myBoard[j+2])
-			or (myBoard[j] == myBoard[j+3] and myBoard[j+3] == myBoard[j+6]))
-		#Diagonals
-			or (j is 0 and myBoard[j] == myBoard[j+4] and myBoard[j+4] == myBoard[j+8])
-			or (j is 2 and myBoard[j] == myBoard[j+2] and myBoard[j+2] == myBoard[j+4])):
-			Winner = myBoard[j]
-			
-def acceptNewMove():
-	global MoveCount
+	#Checks for a winner and if one exists, returns the value of a winner if one exists
+	for j in range(3):				
+		if myBoard[j] == myBoard[j+3] and myBoard[j+3] == myBoard[j+6]:				#Cols
+			return myBoard[j]
+		elif myBoard[3*j] == myBoard[3*j+1] and myBoard[3*j+1] == myBoard[3*j+2]:	#Rows
+			return myBoard[3*j]
+		elif ((myBoard[0] == myBoard[4] and myBoard[4] == myBoard[8])				#Diagonals
+			or (myBoard[2] == myBoard[4] and myBoard[4] == myBoard[6])):
+			return myBoard[4]
+	return " "
+	
+def acceptNewMove(MoveCount):
+	#Prompts user for a valid move 
 	moveIsValid = False
-	while moveIsValid is False:
+	while not moveIsValid:
 		myMove = input("Current player is " + CurrentPlayer + ". Please enter your move: ")
 		try:
 			myMove = int(myMove)
-		except:
-			print("Invalid move, let's try again")
+		except:										#Non-number
+			print("I need a number, let's try again.")
 			continue
-		if myMove > 8 or myMove < 0:
-			print("Invalid move, let's try again")
+		if not str(myMove).strip():					#Null/space entry
+			print("You cannot pass a turn, let's try again.")
+			continue
+		if myMove > 8 or myMove < 0:				#Out of bounds
+			print("Out of bounds selection, let's try again.")
 		else:
 			if BoardState[myMove] is " ":
 				BoardState[myMove] = CurrentPlayer
 				moveIsValid = True
 			else:
-				print("Invalid move, let's try again")
-	MoveCount+=1
+				print("That space is taken, let's try again.")
+	return MoveCount+1
 
-BoardNumbers = range(9)
-BoardState = [" "]*9
-i = 0
-CurrentPlayer = "X"
-MoveCount = 0
-Winner = ""
-
+def initCheckers():
+	#Initializes the checkers variables
+	global BoardNumbers
+	global BoardState
+	global Winner
+	BoardNumbers = range(9)
+	BoardState = [" "]*9
+	Winner = " "
+	
 #Runs the game
-while MoveCount < 9 and Winner is "":
-	print("Legend for where to put game moves")
-	printBoard(BoardNumbers)
-
-	print("")
-	print("Current board state:")
-	printBoard(BoardState)
-	acceptNewMove()
-	checkForWin(BoardState)
+initCheckers()
+MoveCount = 0
+CurrentPlayer = ""
+while MoveCount < 9 and Winner is " ":
 	if CurrentPlayer == "X":
 		CurrentPlayer = "O"
 	else:
 		CurrentPlayer = "X"
-print("")
+	print("Legend for where to put game moves:")
+	printBoard(BoardNumbers)
+	print("\nCurrent board state:")
+	printBoard(BoardState)
+	MoveCount = acceptNewMove(MoveCount)
+	Winner = checkForWin(BoardState)
 
 #Print results of game
-if Winner is not "":
-	print("Game is over! "+Winner+" won in "+str(MoveCount)+" moves!")
-else:
-	print("Game is over, but nobody won =(")
 printBoard(BoardState)
+if Winner is not " ":
+	print("\nGame is over! {} won in {} moves!".format(Winner, MoveCount))
+else:
+	print("\nGame is over, but nobody won =(")
